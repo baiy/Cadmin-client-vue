8f64d3f00898d731a66671013476e763ca232a04
baiy <wo@baiy.org>
2020-09-30 14:05:58 +0800

    添加select组件

---
+++ src/components/fieldSelect.vue
@@ -0,0 +1,65 @@
+<template>
+  <Select :value="value" :style="{width:selectWidth}" :placeholder="defaultDesc" @on-change="changePage" transfer>
+    <Option :value="defaultValue">{{ defaultDesc }}</Option>
+    <Option v-for="item in map" :value="item[valueField]" :key="item[valueField]">{{ item[descField] }}</Option>
+  </Select>
+
+</template>
+<script>
+    export default {
+        name: 'fieldSelect',
+        props: {
+            value: {
+                type: [String, Number],
+                default: function () {
+                    return ''
+                },
+            },
+            map: {
+                type: Array,
+                default: function () {
+                    return []
+                },
+            },
+            valueField: {
+                type: String,
+                default: function () { return 'v'},
+            },
+            descField: {
+                type: String,
+                default: function () { return 'n'},
+            },
+            defaultDesc: {
+                type: String,
+                default: function () {
+                    return '请选择选项'
+                },
+            },
+            defaultValue: {
+                type: [String, Number],
+                default: function () {
+                    return ''
+                },
+            },
+            width: {
+                type: [Number, String],
+                default: function () {
+                    return '100%'
+                },
+            },
+        },
+        computed: {
+            selectWidth () {
+                if (!isNaN(this.width)) {
+                    return this.width + 'px'
+                }
+                return this.width
+            },
+        },
+        methods: {
+            changePage (value) {
+                this.$emit('input', value)
+            },
+        },
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
-import {vueRequest} from './plugins/request'
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
+import { vueRequest } from './plugins/request'
 import store from './store'
 import router from './router'
 import pageBar from './components/pageBar'
@@ -10,21 +10,23 @@ import username from './components/username'
 import authCheck from './components/authCheck'
 import uploadFile from './components/uploadFile'
 import fieldMap from './components/fieldMap'
+import fieldSelect from './components/fieldSelect'
 import App from './App.vue'
 
-Vue.config.productionTip = false;
-Vue.use(ViewUI);
-Vue.use(vueRequest);
+Vue.config.productionTip = false
+Vue.use(ViewUI)
+Vue.use(vueRequest)
 
-Vue.component('page-bar',pageBar);
-Vue.component('username',username);
-Vue.component('auth-check',authCheck);
-Vue.component('table-lists',tableLists);
-Vue.component('upload-file',uploadFile);
-Vue.component('field-map',fieldMap);
+Vue.component('page-bar', pageBar)
+Vue.component('username', username)
+Vue.component('auth-check', authCheck)
+Vue.component('table-lists', tableLists)
+Vue.component('upload-file', uploadFile)
+Vue.component('field-map', fieldMap)
+Vue.component('field-select', fieldSelect)
 
 new Vue({
     router,
     store,
-    render: h => h(App)
-}).$mount('#app');
+    render: h => h(App),
+}).$mount('#app')
